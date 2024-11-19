eb439dacd0954020a179744ccf738a3d19a3ac0e
NhutDang2609 <vothibong961213@gmail.com>
2024-11-19 12:45:57 +0700

    Add files via upload

---
+++ nhutdangbai3.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+	
+//Khai bao bien
+	   int gio;
+	   
+//Nhap gia tri 
+	   printf("Nhap gio: ");
+	   scanf("%d",&gio);
+	   
+//In ra man hinh
+	   if(gio<18)
+		  printf("Have a good day");
+	   else
+	      printf("Have a good night");
+	   	   return 0;		   
+}
