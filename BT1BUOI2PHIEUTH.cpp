4e48a3e4da26b2fa856154c967cef169b9e0b883
NhutDang2609 <vothibong961213@gmail.com>
2024-11-19 18:08:32 +0700

    Add files via upload

---
+++ BT1BUOI2PHIEUTH.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+//Khai bao bien
+	   char hoten[30],lop[10];
+	   
+	   printf("Nhap ho ten: ");
+	   fflush(stdin);
+	   gets(hoten);
+	   printf("Nhap lop: ");
+	   fflush(stdin);
+	   scanf("%s",lop);
+	   return 0;
+}
+
