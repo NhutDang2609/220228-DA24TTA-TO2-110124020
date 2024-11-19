4c39df7cfa39e2ca82bfc352bdc1f226a1944a97
NhutDang2609 <vothibong961213@gmail.com>
2024-11-19 18:48:34 +0700

    Add files via upload

---
+++ BT2BUOI2PHIEUTH.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+//Khai bao bien
+
+int a=100;
+
+float b=8.95;
+
+char c= 'C';
+
+//In du lieu ra man hinh
+
+printf("So nguyen: %d \n",a);
+
+printf("So nguyen: %d \n",b);
+
+printf("Ki tu: %c",c);
+
+return 0;
+
+}
